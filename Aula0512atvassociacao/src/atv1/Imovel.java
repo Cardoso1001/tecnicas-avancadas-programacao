5acc203ae5fc25307f963d853cf1a300ab3cbec5
Cardoso1001 <jaooo.cardoso14@gmail.com>
2026-05-12 14:53:39 -0300

    Atividade dia 12/05 - incompleta

---
+++ Aula0512atvassociacao/src/atv1/Imovel.java
@@ -0,0 +1,23 @@
+package atv1;
+
+public class Imovel {
+    private double valor;
+    private Endereco endereco;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+}
---
+++ Aula0512atvassociacao/src/atv1/Proprietario.java
@@ -0,0 +1,14 @@
+package atv1;
+
+public class Proprietario extends Pessoa {
+    private String conjuge;
+
+    public String getConjuge() {
+        return conjuge;
+    }
+
+    public void setConjuge(String conjuge) {
+        this.conjuge = conjuge;
+    }
+    
+}
